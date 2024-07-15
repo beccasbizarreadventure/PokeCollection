a62c018e0671a4214c7000808137a28e1be82dd0
Rebecca <rebecca90469@gmail.com>
2024-07-14 21:54:44 -0400

    Pokemonlist styling mostly done

--- src/Pokemon/components/PokemonsList.jsx
+++ src/Pokemon/components/PokemonsList.jsx
@@ -20,20 +20,34 @@ const PokemonsList = () => {
             <tr
               className={`flex flex-col justify-center items-center ${
                 colors[`${pokemon.type}`]
-              } w-300px h-300px rounded-t-lg rounded-b-lg m-5 py-5 px-10`}
+              } rounded shadow-lg m-5 py-5 px-10`}
               key={pokemon.id}
             >
               <td>
                 <img
-                  className="-z-0"
                   src={pokemon.imageUrl}
                   alt={pokemon.name}
                   style={{ width: "100px", height: "100px" }}
                 />
               </td>
-              <td className="font-bold text-white text-xl mb-2">{capitalizePokemonName(`${pokemon.name}`)}</td>
+              <td className="font-bold text-white text-xl mb-2 mt-2">
+                {capitalizePokemonName(`${pokemon.name}`)}
+              </td>
               <td>
-                <button onClick={() => handleCapture(pokemon)} className="bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded-full">capture</button>
+                <button onClick={() => handleCapture(pokemon)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    class="size-8"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
               </td>
             </tr>
           ))}
